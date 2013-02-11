38775035fdefa7e7eaaccee45fc889d126fd9b43
Scottyhurlow@gmail.com <Jester04>
2013-02-10 21:42:09 -0600

    close

--- src/edu/macalester/cs124/fallingbox/FallingBox.java
+++ src/edu/macalester/cs124/fallingbox/FallingBox.java
@@ -1,19 +1,36 @@
 package edu.macalester.cs124.fallingbox;
 
+import java.awt.Color;
+import java.awt.Point;
+
 import acm.graphics.*;
 import acm.program.*;
 
 public class FallingBox extends GraphicsProgram {
-    
+    ;
     public void run() {
+    	GCanvas canvas = new GCanvas();
+    	setBackground(Color.cyan);
         GRect square = new GRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
         square.setFilled(true);
         add(square);
         double dx = (getWidth()  - SQUARE_SIZE) / N_STEPS;
         double dy = (getHeight() - SQUARE_SIZE) / N_STEPS;
+        
         for(int i = 0; i < N_STEPS; i++) {
-            square.move(dx, dy);
-            pause(PAUSE_TIME);
+        	
+        	if (square.getY() < (getHeight() - 50)) {
+        		square.move(dx, dy);
+        		dy = dy + 0.02;
+        		pause(PAUSE_TIME);
+        	}
+        	else {
+        		square.setColor(Color.magenta);
+        		square.move(dx, dy);
+        		dx = dx - 0.02;
+        			
+        		
+            }    
         }
     }
 
